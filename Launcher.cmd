5a40a2bf89fcafe7c876faec173c7c77419252f5
Sysel19 <45149494+Sysel19@users.noreply.github.com>
2018-11-18 19:13:33 +0100

    Create Launcher.cmd

---
+++ Launcher.cmd
@@ -0,0 +1,11 @@
+@echo off
+title SMV the launcher
+:: Dam sem poznamky abych se cejtil dobre
+
+:virmessage
+
+choice /c AN /m "Jsi si jisty ze toto neni vir ?"
+if %ERRORLEVEL%==a goto :MENU
+if %ERRORLEVEL%==n exit
+
+:MENU
