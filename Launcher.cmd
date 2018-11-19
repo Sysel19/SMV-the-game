7723ff75e07d005537761daa467d505c1e468465
MarkorN312 <hcrafteuremarkorn@seznam.cz>
2018-11-19 14:52:35 +0100

    Update Launcher.cmd #1
    "Novinky":
    Upravené (nedodělané) menu v launcher.cmd

--- Launcher.cmd
+++ Launcher.cmd
@@ -11,5 +11,21 @@ if %ERRORLEVEL%==n exit
 :MENU
 
 cls
-echo.
-// test
+echo *-----------------------------------------------------------*
+echo /                                                           \
+echo \   ///////////        ////     ////     //            //   /
+echo /  //                 // //    // //      //          //    \
+echo \  //                //   //  //  //      //        //      /
+echo /  ///////////      //     ////   //      //        //      \
+echo \           //      //            //       //     //        /
+echo /           //      //            //        //  //          \
+echo \  //////////      //             //         ///            /
+echo./                                                           \
+echo *-----------------------------------------------------------*
+echo 1 - Start hry
+echo 2 - Napoveda
+echo 3 - Konec hry
+choice /c 123 /m "Vyber si jednu z moznosti:"
+if %ERRORLEVEL%==1 goto :savy
+if %ERRORLEVEL%==2 goto :napoveda
+if %ERRORLEVEL%==3 exit
