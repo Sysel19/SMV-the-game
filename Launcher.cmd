55aa785f9ef016d8d79a24e29a7a91896862cacd
MarkorN312 <45149444+MarkorN312@users.noreply.github.com>
2018-11-19 17:23:37 +0100

    Update Launcher.cmd

--- Launcher.cmd
+++ Launcher.cmd
@@ -5,27 +5,8 @@ title SMV the launcher
 :virmessage
 
 choice /c AN /m "Jsi si jisty ze toto neni vir ?"
-if %ERRORLEVEL%==a goto :MENU
+if %ERRORLEVEL%==a goto :START
 if %ERRORLEVEL%==n exit
 
-:MENU
-
-cls
-echo *-----------------------------------------------------------*
-echo /                                                           \
-echo \   ///////////        ////     ////     //            //   /
-echo /  //                 // //    // //      //          //    \
-echo \  //                //   //  //  //      //        //      /
-echo /  ///////////      //     ////   //      //        //      \
-echo \           //      //            //       //     //        /
-echo /           //      //            //        //  //          \
-echo \  //////////      //             //         ///            /
-echo./                                                           \
-echo *-----------------------------------------------------------*
-echo 1 - Start hry
-echo 2 - Napoveda
-echo 3 - Konec hry
-choice /c 123 /m "Vyber si jednu z moznosti:"
-if %ERRORLEVEL%==1 goto :savy
-if %ERRORLEVEL%==2 goto :napoveda
-if %ERRORLEVEL%==3 exit
+:START
+start menu.cmd
