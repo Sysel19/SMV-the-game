5d26dfd5579342bcd543065a4866732f8eefe32c
MarkorN312 <45149444+MarkorN312@users.noreply.github.com>
2018-11-19 17:24:04 +0100

    Update menu.cmd

--- menu.cmd
+++ menu.cmd
@@ -1,1 +1,23 @@
-@echo off
+goto :MENU
+
+:MENU
+
+cls
+echo *-----------------------------------------------------------*
+echo /                                                           \
+echo \   ///////////        ////     ////     //            //   /
+echo /  //                 // //    // //      //          //    \
+echo \  //                //   //  //  //      //        //      /
+echo /  ///////////      //     ////   //      //        //      \
+echo \           //      //            //       //     //        /
+echo /           //      //            //        //  //          \
+echo \  //////////      //             //         ///            /
+echo./                                                           \
+echo *-----------------------------------------------------------*
+echo 1 - Start hry
+echo 2 - Napoveda
+echo 3 - Konec hry
+choice /c 123 /m "Vyber si jednu z moznosti:"
+if %ERRORLEVEL%==1 goto :savy
+if %ERRORLEVEL%==2 goto :napoveda
+if %ERRORLEVEL%==3 exit
