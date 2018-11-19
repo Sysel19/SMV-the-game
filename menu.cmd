76d873c5cdceb6da9639b234e0d5273900a3f177
MarkorN312 <hcrafteuremarkorn@seznam.cz>
2018-11-19 18:11:58 +0100

    Update menu.cmd
    Kompletně funkční menu I guess.

--- menu.cmd
+++ menu.cmd
@@ -1,4 +1,5 @@
 @echo off
+color a
 title SMV the menu
 goto :1MENUStart
 
@@ -21,7 +22,8 @@ echo * 1 - Start hry *
 echo 2 - Napoveda
 echo 3 - Konec hry
 choice /c 123 /m "Vyber si jednu z moznosti:"
-if %ERRORLEVEL%==1 goto :savy
+if %ERRORLEVEL%==1 (start savy.cmd
+exit)
 if %ERRORLEVEL%==2 goto :2MENUNapoveda
 if %ERRORLEVEL%==3 goto :3MENUKonec
 
@@ -45,7 +47,7 @@ echo * 2 - Napoveda *
 echo 3 - Konec hry
 choice /c 123 /m "Vyber si jednu z moznosti:"
 if %ERRORLEVEL%==1 goto :1MENUStart
-if %ERRORLEVEL%==2 goto :
+if %ERRORLEVEL%==2 goto :Napoveda
 if %ERRORLEVEL%==3 goto :3MENUKonec
 
 :3MENUKonec
@@ -70,3 +72,16 @@ choice /c 123 /m "Vyber si jednu z moznosti:"
 if %ERRORLEVEL%==1 goto :1MENUStart
 if %ERRORLEVEL%==2 goto :2MENUNapoveda
 if %ERRORLEVEL%==3 exit
+
+:Napoveda
+
+cls
+echo Pohyb: "w,a,s,d"
+timeout /t 1 >nul /nobreak
+echo Pohyb v menu 1,2,3
+timeout /t 1 >nul /nobreak
+echo Interakce "e"
+timeout /t 1 >nul /nobreak
+echo Navrat do menu "b"
+timeout /t 1 >nul /nobreak
+goto :2MENUNapoveda
