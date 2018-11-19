306439898aef31e132d3b0b51e9755c8a87f6895
MarkorN312 <45149444+MarkorN312@users.noreply.github.com>
2018-11-19 17:51:39 +0100

    Update menu.cmd
    Pohyb v menu.

--- menu.cmd
+++ menu.cmd
@@ -1,6 +1,8 @@
-goto :MENU
+@echo off
+title SMV the menu
+goto :1MENUStart
 
-:MENU
+:1MENUStart
 
 cls
 echo *-----------------------------------------------------------*
@@ -14,10 +16,57 @@ echo /           //      //            //        //  //          \
 echo \  //////////      //             //         ///            /
 echo./                                                           \
 echo *-----------------------------------------------------------*
-echo 1 - Start hry
+echo.
+echo * 1 - Start hry *
 echo 2 - Napoveda
 echo 3 - Konec hry
 choice /c 123 /m "Vyber si jednu z moznosti:"
 if %ERRORLEVEL%==1 goto :savy
-if %ERRORLEVEL%==2 goto :napoveda
+if %ERRORLEVEL%==2 goto :2MENUNapoveda
+if %ERRORLEVEL%==3 goto :3MENUKonec
+
+:2MENUNapoveda
+
+cls
+echo *-----------------------------------------------------------*
+echo /                                                           \
+echo \   ///////////        ////     ////     //            //   /
+echo /  //                 // //    // //      //          //    \
+echo \  //                //   //  //  //      //        //      /
+echo /  ///////////      //     ////   //      //        //      \
+echo \           //      //            //       //     //        /
+echo /           //      //            //        //  //          \
+echo \  //////////      //             //         ///            /
+echo./                                                           \
+echo *-----------------------------------------------------------*
+echo.
+echo 1 - Start hry
+echo * 2 - Napoveda *
+echo 3 - Konec hry
+choice /c 123 /m "Vyber si jednu z moznosti:"
+if %ERRORLEVEL%==1 goto :1MENUStart
+if %ERRORLEVEL%==2 goto :
+if %ERRORLEVEL%==3 goto :3MENUKonec
+
+:3MENUKonec
+
+cls
+echo *-----------------------------------------------------------*
+echo /                                                           \
+echo \   ///////////        ////     ////     //            //   /
+echo /  //                 // //    // //      //          //    \
+echo \  //                //   //  //  //      //        //      /
+echo /  ///////////      //     ////   //      //        //      \
+echo \           //      //            //       //     //        /
+echo /           //      //            //        //  //          \
+echo \  //////////      //             //         ///            /
+echo./                                                           \
+echo *-----------------------------------------------------------*
+echo.
+echo 1 - Start hry
+echo 2 - Napoveda
+echo * 3 - Konec hry *
+choice /c 123 /m "Vyber si jednu z moznosti:"
+if %ERRORLEVEL%==1 goto :1MENUStart
+if %ERRORLEVEL%==2 goto :2MENUNapoveda
 if %ERRORLEVEL%==3 exit
