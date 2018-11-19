885b0eaa73a14d2ad4f5972381ce5a199eea0711
Sysel19 <45149494+Sysel19@users.noreply.github.com>
2018-11-19 18:46:43 +0100

    Update savy.cmd

--- savy.cmd
+++ savy.cmd
@@ -1,6 +1,11 @@
 @echo off
 title SMV the loader 
 set /p lokace=<lokace.txt
-set /p kapitola=<kapitola.txt
-set /p questhotovo=<questhotovo
-set /p questrunning=<questrunning
+if %lokace%==les (start les.cmd exit)
+if %lokace%==hory (start hory.cmd exit)
+if %lokace%==poust (start poust.cmd exit)
+if %lokace%==jezero (start jezero.cmd exit)
+if %lokace%==plane (start plane.cmd exit)
+if %lokace%==mesto (start mesto.cmd exit)
+if %lokace%==bilyhrad (start bylihrad.cmd exit)
+if %lokace%==cernyhrad (start cernyhrad.cmd exit)
